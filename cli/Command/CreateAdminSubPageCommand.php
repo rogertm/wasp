393547858224962a0b598fe5d2d67e5ae8518b08
rogertm <roger213tm@gmail.com>
2025-05-21 16:12:28 -0400

    Command: Create Admin Sub Page php cli/wasp create:admin_subpage "Child Dashboard" wasp-dashboard-setting

--- cli/Command/CreateAdminSubPageCommand.php
+++ cli/Command/CreateAdminSubPageCommand.php
@@ -6,15 +6,16 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class CreateAdminPageCommand extends AbstractGeneratorCommand
+class CreateAdminSubPageCommand extends AbstractGeneratorCommand
 {
-	protected static $defaultName = 'create:admin_page';
+	protected static $defaultName = 'create:admin_subpage';
 
 	protected function configure(): void
 	{
 		$this
 			->setDescription('Creates a new Admin Page class file using project config')
-			->addArgument('name', InputArgument::REQUIRED, 'Admin Page name (e.g., My plugin dashboard)');
+			->addArgument('name', InputArgument::REQUIRED, 'Admin Page name (e.g., My plugin dashboard)')
+			->addArgument('parent_slug', InputArgument::REQUIRED, 'The slug name for the parent menu (e.g., wasp-dashboard-setting)');
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output): int
@@ -22,6 +23,7 @@ class CreateAdminPageCommand extends AbstractGeneratorCommand
 		$this->initialize($input, $output);
 
 		$name			= $input->getArgument('name');
+		$parent_slug	= $input->getArgument('parent_slug');
 		$slug			= $this->slugify($name);
 		$classSuffix	= str_replace('-', '_', ucwords($slug, '-'));
 		$className		= 'Admin_Page_' . $classSuffix;
@@ -41,33 +43,33 @@ class CreateAdminPageCommand extends AbstractGeneratorCommand
 
 		$namespaceDecl	= $this->namespaceRoot . '\\Admin';
 		$useDecl		= $this->namespaceRoot . '\\Admin\\Admin_Page';
-		$page_slug 		= $this->slugRoot.'-'.$slug.'-setting';
+		$page_slug 		= $this->slugRoot.'-'.$slug.'-subsetting';
 		$content		= <<<PHP
 <?php
 namespace {$namespaceDecl};
 
 use {$useDecl};
 
-class {$className} extends Admin_Page
+class {$className} extends Admin_Sub_Menu_Page
 {
 	public function __construct()
 	{
 		parent::__construct();
+		\$this->parent_slug		= '{$parent_slug}';
 		\$this->page_title		= __( '{$name} Admin Page', '{$this->textDomain}' );
 		\$this->menu_title		= __( '{$name}', '{$this->textDomain}' );
-		\$this->page_heading	= __( '{$name} Admin Page', '{$this->textDomain}' );
+		\$this->page_heading	= __( '{$name} Submenu Dashboard', '{$this->textDomain}' );
 		\$this->capability		= 'manage_options';
 		\$this->menu_slug		= '{$page_slug}';
-		\$this->option_group	= '{$this->slugRoot}_setting';
+		\$this->option_group	= '{$this->slugRoot}_subsetting';
 		\$this->option_name		= '{$this->slugRoot}_options';
-		\$this->position		= 2;
 	}
 }
 
 PHP;
 
 		file_put_contents($filePath, $content);
-		$output->writeln("Created Admin Page class file: $filePath");
+		$output->writeln("Created Admin Sub Page class file: $filePath");
 
 		$loaderFile = $this->baseDir . '/inc/classes.php';
 		$instanceLine = sprintf(
