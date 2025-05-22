dfa93a62f8b8b2db7949081b243bd2198cfc0931
rogertm <roger213tm@gmail.com>
2025-05-22 18:44:30 -0400

    Command: Create Setting Fields php cli/wasp create:setting_fields "Config" wasp-dashboard-setting <subpage>

---
+++ cli/Command/CreateSettingFieldsCommand.php
@@ -0,0 +1,103 @@
+<?php
+namespace WaspCli\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CreateSettingFieldsCommand extends AbstractGeneratorCommand
+{
+	protected static $defaultName = 'create:setting_fields';
+
+	protected function configure(): void
+	{
+		$this
+			->setDescription('Creates a new Settings Fields class file using project config')
+			->addArgument('section', InputArgument::REQUIRED, 'Section name (e.g., My section fields)')
+			->addArgument('page_slug', InputArgument::REQUIRED, 'The slug-name of the settings page on which to show the section. (e.g., wasp-dashboard-setting)')
+			->addArgument('is_subpage', InputArgument::OPTIONAL, 'If the setting fields are to be displayed on a sub page, this value must be specified as "subpage", default is null', null);
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output): int
+	{
+		$this->initialize($input, $output);
+
+		$section		= $input->getArgument('section');
+		$page_slug		= $input->getArgument('page_slug');
+		$is_subpage		= $input->getArgument('is_subpage');
+		$slug			= $this->slugify($section);
+		$classSuffix	= str_replace('-', '_', ucwords($slug, '-'));
+		$className		= 'Setting_Fields_' . $classSuffix;
+		$fileName		= "class-{$this->slugRoot}-setting-fields-{$slug}.php";
+
+		$targetDir = $this->baseDir . '/classes/setting-fields';
+		if (!is_dir($targetDir)) {
+			mkdir($targetDir, 0755, true);
+		}
+		$filePath = $targetDir . '/' . $fileName;
+
+		// Check if file already exists to avoid overwriting
+		if (file_exists($filePath)) {
+			$output->writeln("<error>File already exists: $filePath</error>");
+			return Command::FAILURE;
+		}
+
+		$namespaceDecl	= $this->namespaceRoot . '\\Setting_Fields';
+		$useDecl		= $this->namespaceRoot . '\\Setting_Fields\\Setting_Fields';
+		$filter			= str_replace('-', '_', $slug);
+		$subpage 		= ('subpage' === $is_subpage) ? 'sub' : null;
+		$content		= <<<PHP
+<?php
+namespace {$namespaceDecl};
+
+use {$useDecl};
+
+class {$className} extends Setting_Fields
+{
+	public function __construct()
+	{
+		parent::__construct();
+		\$this->slug 			= '{$page_slug}';
+		\$this->option_group 	= '{$this->slugRoot}_{$subpage}setting';
+		\$this->option_name 		= '{$this->slugRoot}_{$subpage}options';
+		\$this->section_id 		= '{$slug}-section-id';
+		\$this->section_title 	= __( '{$section}', '{$this->textDomain}' );
+		\$this->field_id 		= '{$slug}-field-id';
+		\$this->field_title 		= __( '{$section} fields', '{$this->textDomain}' );
+	}
+
+	function fields()
+	{
+		\$fields = array(
+			// Your fields goes here...
+		);
+
+		return apply_filters( '{$this->slugRoot}_{$filter}_custom_fields', \$fields );
+	}
+}
+
+PHP;
+
+		file_put_contents($filePath, $content);
+		$output->writeln("Created Setting Fields class file: $filePath");
+
+		$loaderFile = $this->baseDir . '/inc/classes.php';
+		$instanceLine = sprintf(
+		    "new %s\\Setting_Fields\\%s;\n",
+		    $this->namespaceRoot,
+		    $className
+		);
+
+		if (file_exists($loaderFile) && is_writable($loaderFile)) {
+		    file_put_contents($loaderFile, $instanceLine, FILE_APPEND);
+		    $output->writeln("Appended instance to: $loaderFile");
+		} else {
+		    $output->writeln("<comment>Warning: Could not write to $loaderFile</comment>");
+		}
+
+		$output->writeln('<info>Done!</info>');
+		return Command::SUCCESS;
+
+	}
+}
